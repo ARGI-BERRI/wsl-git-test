a094081de1279e147cabbb6a0a42fff1551c356b
ARGI-BERRI <me@argia.dev>
2023-04-15 10:12:15 +0900

    change: test

--- src/test1.py
+++ src/test1.py
@@ -1,12 +1,25 @@
-for year in [1900, 1901, 2000, 2004]:
-    print(f"What if {year}?")
+def main():
+    for year in [1900, 1901, 2000, 2004]:
+        print(f"What if {year}?")
 
-    if year % 4 != 0:
-        print(f"{year}年は平年")
-    elif (
-        year % 100 != 0
-        or year % 400 == 0
-    ):
-        print(f"{year}年は閏年")
-    else:
-        print(f"{year}年は平年")
+        if year % 4 != 0:
+            print(normal_year(year))
+        elif (
+            year % 100 != 0
+            or year % 400 == 0
+        ):
+            print(leap_year(year))
+        else:
+            print(normal_year(year))
+
+
+def leap_year(year: int) -> str:
+    return f"{year}は閏年"
+
+
+def normal_year(year: int) -> str:
+    return f"{year}は平年"
+
+
+if __name__ == "__main__":
+    main()
