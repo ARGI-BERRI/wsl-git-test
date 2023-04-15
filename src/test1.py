a7dde0f51f7d34018b6f4aeea0321e9d8d56d2c4
ARGI-BERRI <124055639+ARGI-BERRI@users.noreply.github.com>
2023-04-15 09:23:03 +0900

    add: test python file

---
+++ src/test1.py
@@ -0,0 +1,12 @@
+year = 100
+
+for year in [1900, 1901, 2000, 2004]:
+    if year % 4 != 0:
+        print(f"{year}年は平年")
+    elif (
+        year % 100 != 0
+        or year % 400 == 0
+    ):
+        print(f"{year}年は閏年")
+    else:
+        print(f"{year}年は平年")
