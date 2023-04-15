0a608c3bb172f0c4fe4ff56fbbff4b8017b179f9
ARGI-BERRI <me@argia.dev>
2023-04-15 09:48:50 +0900

    change: test

--- src/test1.py
+++ src/test1.py
@@ -1,6 +1,6 @@
-year = 100
-
 for year in [1900, 1901, 2000, 2004]:
+    print(f"What if {year}?")
+
     if year % 4 != 0:
         print(f"{year}年は平年")
     elif (
@@ -10,5 +10,3 @@ for year in [1900, 1901, 2000, 2004]:
         print(f"{year}年は閏年")
     else:
         print(f"{year}年は平年")
-
-print(year)
